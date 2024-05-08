c5bed59150f3a82477c880345e0c0ff1be21bd09
yogeshprakhar <yogeshprakharpal200027@gmail.com>
2024-05-08 21:33:18 +0530

    remove credentials

--- src/api/MyUserApi.tsx
+++ src/api/MyUserApi.tsx
@@ -17,7 +17,6 @@ export const useGetMyUser = () => {
         Authorization: `Bearer ${accessToken}`,
         "Content-Type": "application/json",
       },
-      credentials: "include",
     });
 
     if (!response.ok) {
